cafa28dd80e1296bee2ee72a7b6d6aa2a3dd5abd
Alberto Ribeiro <albertotxar@hotmail.com>
2023-09-19 19:20:13 -0300

    Atualizadas as funcoes de manipulacao de parametros do backend

--- server/index.js
+++ server/index.js
@@ -1,67 +1,57 @@
 import cors from "cors"
 import express from "express"
 import { dynamodb } from "./aws.js"
+import { getUserParam, getParam, newParam, updateParam } from "./param.js"
+//import { v4 as uuidv4 } from "uuid";
+import { randomNumber } from "./calc.js"
 
 const app = express()
 app.use(express.json())
 app.use(cors())
 
-app.get('/get-item/:id', async (req, res) => {
-    const params = {
-      TableName: 'creditchecker',
-      Key: {
-        // Especifique a chave primária do item que você deseja buscar
-        id: req.params.id,
-      },
-    }
-  
-    try {
-      const data = await dynamodb.get(params).promise()
-      res.json(data.Item)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'Erro ao buscar item no DynamoDB' })
-    }
-  })
+app.get("/getparam/:id", async (req, res) => {
+  getParam(req, res, req.params.id)
+})
 
-  app.post('/params', async (req, res) => {
-    const params = {
-      TableName: 'creditchecker',
-      Item: req.body
-      }
-  
-    try {
-      const data = await dynamodb.put(params).promise()
-      res.json(data.Item)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'Erro ao inserir dados no DynamoDB' })
-    }
-  })
+app.get("/params/:userId", async (req, res) => {
+  getUserParam(req, res, req.params.userId)
+})
 
-  app.post('/test', async (req, res) => {
-    const params = {
-      TableName: 'creditchecker',
-      Item: 
-      {
-        id: "0", 
-        userId: "010203",
-        taxa: 7.00,
-        idade: "18-25", 
-        salario: "1000.00-1500.00",
-        tipoFinanciamento: "Teste", 
-        valFinanciamento: "2500.00-3000.00",
-        correntista: true
-      }
-      }
-  
-    try {
-      const data = await dynamodb.put(params).promise()
-      res.json(data.Item)
-    } catch (error) {
-      console.error(error)
-      res.status(500).json({ error: 'Erro ao inserir dados no DynamoDB' })
+app.post("/params/new", async (req, res) => { 
+  newParam(req, res)
+})
+
+app.patch("/params/update", async (req, res) => { 
+  updateParam(req, res)
+})
+
+// Apenas para testes de criação no banco de dados.
+app.post("/test", async (req, res) => {
+  const params = {
+    TableName: "creditchecker",
+    Item: 
+    {
+      id: "test", 
+      userId: "test",
+      taxa: randomNumber(1, 10),
+      idadeMin: 18, 
+      idadeMax: randomNumber(20, 25),
+      salarioMin: randomNumber(1000, 1500),
+      salarioMax: randomNumber(2000, 3000),
+      tipoFinanciamento: "Teste", 
+      valFinancMin: randomNumber(2500, 3000),
+      valFinancMax: randomNumber(3500, 5000),
+      correntista: true
     }
-  })
+    }
+
+  try {
+    const data = await dynamodb.put(params).promise()
+    res.json(data.Item)
+  } catch (error) {
+    console.error(error)
+    res.status(500).json({ error: "Erro ao inserir dados no DynamoDB" })
+  }
+})
 
 app.listen(3333, () => console.log("Server is running on port 3333"))
---
+++ server/param.js
@@ -0,0 +1,92 @@
+import { dynamodb } from "./aws.js"
+
+// Função para retornar todos os dados de um determinado item (parametro) através do seu id.
+// Exemplo de uso: id = 1, acessar http://localhost:3333/getitem/1 - método GET
+export async function getParam(req, res, paramId) {
+    const params = {
+        TableName: "creditchecker",
+        Key: {
+          id: paramId
+        },
+      }
+    
+      try {
+        const data = await dynamodb.get(params).promise()
+        console.log(data)
+        res.json(data.Item)
+      } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Erro ao buscar item no DynamoDB" })
+      }
+}
+
+// Função para retornar todos os parametros relacionados ao usuário informado através do userId.
+// Exemplo de uso: userId = 2, acessar http://localhost:3333/params/2 - método GET
+export async function getUserParam(req, res, userId) {
+    const params = {
+        TableName: "creditchecker",
+        FilterExpression: "userId = :userIdValue",
+        ExpressionAttributeValues: {
+            ":userIdValue": userId,
+        },
+      }
+    
+      try {
+        const data = await dynamodb.scan(params).promise()
+        console.log(data)
+        res.json(data.Items)
+      } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Erro ao buscar item no DynamoDB" })
+      }
+}
+
+// Função para criar um novo parametros através do corpo da requisição.
+// Exemplo de uso: Enviar no corpo da requisição em http://localhost:3333/params/new -  método POST
+// Formato: JSON
+// {"id":"1",
+// "userId":1,
+// "idadeMin":18,"idadeMax":21,
+// "salarioMin":1068,"salarioMax":2285,
+// "tipoFinanciamento":"Teste",
+// "valFinancMin":2844,"valFinancMax":3546,
+// "correntista":true,
+// "taxa":3}
+export async function newParam(req, res) {
+    const params = {
+        TableName: "creditchecker",
+        Item: req.body
+        }
+    
+      try {
+        const data = await dynamodb.put(params).promise()
+        res.json(data.Item)
+      } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Erro ao incluir item no DynamoDB" })
+      }
+}
+
+// Função para atualizar um parametro através do corpo da requisição
+// Exemplo de uso: Enviar no corpo da requisição o id e a taxa em http://localhost:3333/params/update -  método PATCH
+export async function updateParam(req, res) {
+    const params = {
+        TableName: "creditchecker",
+        Key: {
+            id: req.body.id,
+          },
+          UpdateExpression: "SET taxa = :newTaxa",
+          ExpressionAttributeValues: {
+            ":newTaxa": req.body.taxa,
+          },
+          ReturnValues: "ALL_NEW"
+        }
+
+      try {
+        const data = await dynamodb.update(params).promise()
+        res.json(data.Item)
+      } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Erro ao atualizar item no DynamoDB" })
+      }
+}
