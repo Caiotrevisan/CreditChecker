27027351becf08ef93b395166bd27dccaee060bc
Alberto Ribeiro <albertotxar@hotmail.com>
2023-09-23 23:33:05 -0300

    Atualizada a rota param.js para aceitar envio de mais de um parametro na criação.

--- server/param.js
+++ server/param.js
@@ -27,15 +27,19 @@ export async function getParam(req, res, paramId) {
 
 /*
 Função para retornar todos os parametros relacionados ao usuário informado através do userId.
-Exemplo de uso: userId = 2, acessar http://localhost:3333/params/2 - método GET
+Exemplo de uso: Enviar no corpo da requisição em http://localhost:3333/params - método POST
+Formato: JSON
+{
+   "userId": "1"
+}
 */
-export async function getUserParam(req, res, userId) {
+export async function getUserParam(req, res) {
     const params = {
         TableName: "creditchecker",
         FilterExpression: "itemType = :itemTypeValue AND userId = :userIdValue",
         ExpressionAttributeValues: {
           ":itemTypeValue": itemType,
-          ":userIdValue": userId,
+          ":userIdValue": req.body.userId,
         },
       }
 
@@ -49,39 +53,43 @@ export async function getUserParam(req, res, userId) {
 }
 
 /*
-Função para criar um novo parametros através do corpo da requisição.
+Função para criar novos parametros através do corpo da requisição.
 Exemplo de uso: Enviar no corpo da requisição em http://localhost:3333/params/new -  método POST
-Formato: JSON
-{
-   "id": "1",
-   "userId": 1,
-   "ageMin": 18,
-   "ageMax": 21,
-   "salaryMin": 1068,
-   "salaryMax": 2285,
-   "financingType": "Teste",
-   "financValMin": 2844,
-   "financValMax": 3546,
-   "client": true,
-   "fee": 3
-}
+Formato: JSON (Obs: Será um array, pois pode conter um ou mais parametros)
+[
+  {
+    "id": "1",
+    "userId": 1,
+    "ageMin": 18,
+    "ageMax": 21,
+    "salaryMin": 1068,
+    "salaryMax": 2285,
+    "financingType": "Teste",
+    "financValMin": 2844,
+    "financValMax": 3546,
+    "client": true,
+    "fee": 3
+  }
+]
 */
 export async function newParam(req, res) {
-  const params = {
-    TableName: "creditchecker",
-    Item: {
-      id: uuid(),
-      itemType: itemType,
-      ...req.body
-    }}
+  req.body.forEach(async value => {
+    const params = {
+      TableName: "creditchecker",
+      Item: {
+        id: uuid(),
+        itemType: itemType,
+        ...value
+      }}
 
-  try {
-    const data = await dynamodb.put(params).promise()
-    res.json(data.Item)
-  } catch (error) {
-    console.error(error)
-    res.status(500).json({ error: "Erro ao incluir item no banco de dados" })
-  }
+    try {
+      const data = await dynamodb.put(params).promise()
+      res.json(data.Item)
+    } catch (error) {
+      console.error(error)
+      res.status(500).json({ error: "Erro ao incluir item no banco de dados" })
+    }
+  });
 }
 
 /*
