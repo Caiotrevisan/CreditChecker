814ff3a856977170d373880fc2388b7f4b504edc
victorpereiira <victorrpsantos@gmail.com>
2023-09-24 23:01:48 -0300

    HotFix: HomeUser - modal

--- src/pages/Home/HomeUser/index.tsx
+++ src/pages/Home/HomeUser/index.tsx
@@ -348,6 +348,10 @@ padding: 5px 10px;
 `
 
 const Modal = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
   display: flex;
   align-items: center;
   padding: 10px;
