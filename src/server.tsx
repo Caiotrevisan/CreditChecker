53fd5252bdce0ce536b8947c0d577a3460cfe966
Alberto Ribeiro <albertotxar@hotmail.com>
2023-11-15 22:57:41 -0300

    Atualizado arquivo de conexão para backend.

--- src/server.tsx
+++ src/server.tsx
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const server = axios.create({
-  baseURL: "http://localhost:80",
+  baseURL: "http://ec2-54-232-247-69.sa-east-1.compute.amazonaws.com:80",
 }) 
 
 export default server
